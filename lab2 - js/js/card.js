69166e46fe58eded7e9c4f143646b39f94b703b1
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-09-25 13:22:09 +0200

    feat: fix render cards

--- lab2 - js/js/card.js
+++ lab2 - js/js/card.js
@@ -21,8 +21,10 @@ export default class Card {
     console.log("Rendering card...");
 
     // 🔥🔥🔥 TODO3: build the HTML element and append it to the DOM
-    // let card = document.createElement("div");
-    // card.dataset.number = counter + 1;
+    let card = document.createElement("div");
+    card.dataset.number = counter + 1;
+    card.appendChild()
+
     // don't forget to append the child to to DOM
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
