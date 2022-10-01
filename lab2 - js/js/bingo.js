12ca23f88b5a5bfe1f3c4f79302a432587533eae
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-01 10:59:49 +0200

    fix: change code todo 2

--- lab2 - js/js/bingo.js
+++ lab2 - js/js/bingo.js
@@ -61,8 +61,7 @@ export default class Bingo {
 
     // 🔥🔥🔥 TODO 6
     // count all cards that are marked as done (select done items and count them with .length)
-    let cardsDone = document.getElementsByClassName("bingo__card--done").length;
-    console.log(cardsDone);
+    let cardsDone = document.querySelectorAll("bingo__card--done");
     if (cardsDone.length === 5) {
     // show the animated gif to the winner
       document.querySelector(".bingo__overlay").style.display = "block";
