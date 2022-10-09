44c2356484d722a2e70c5481242b511fb32f9202
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-09 16:21:45 +0200

    feat: add class Weather

--- lab3 - api/lab3 api/main.js
+++ lab3 - api/lab3 api/main.js
@@ -1,1 +1,5 @@
 import './style.css'
+//import weather class
+import Weather from './weather.js'
+
+let weather = new Weather();
---
+++ lab3 - api/lab3 api/weather.js
@@ -0,0 +1,8 @@
+
+export default class Weather {
+    constructor() {
+        this.lat = 0;
+        this.lon = 0;
+        navigator.geolocation.getCurrentPosition(this.getCoords.bind(this));
+    }
+}
