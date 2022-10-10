44bf2d1c9e02ff4b1e3b628f5a128c57db682888
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-10 12:24:24 +0200

    feat: add fetch method

--- lab3 - api/lab3 api/weather.js
+++ lab3 - api/lab3 api/weather.js
@@ -66,6 +66,11 @@ export default class Weather {
             method: 'GET',
             headers: {'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY', 'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'}
         };
+
+        fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
     }
 
     //fetch data from gutenburg api
