4300c5fa5fe39fce7be09973b590eae655d585b8
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-16 11:31:34 +0200

    feat: import three and orbitcontrols

--- lab4/main.js
+++ lab4/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+//import three
+import * as THREE from 'three'
+//import OrbitControls
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
+//create scene
+const scene = new THREE.Scene()
