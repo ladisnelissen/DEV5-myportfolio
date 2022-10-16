6c31dd95c22679d65ba669bd4f34981c0a7eef1d
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-16 11:33:41 +0200

    feat: add directional light

--- lab4/main.js
+++ lab4/main.js
@@ -24,3 +24,9 @@ scene.add(gridHelper);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
+//add directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(0, 1, 0);
+scene.add(directionalLight);
+
+
