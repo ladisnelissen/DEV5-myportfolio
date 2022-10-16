825ea2e3adeedb44f78650c01c9b43f1a8f6448c
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-10-16 11:32:45 +0200

    feat: add renderer

--- lab4/main.js
+++ lab4/main.js
@@ -7,3 +7,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 //create scene
 const scene = new THREE.Scene()
+
+//setup camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+//setup renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
